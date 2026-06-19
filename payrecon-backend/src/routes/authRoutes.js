a6517f5e38a8690eec95c974cd69ae5dee9cbd34
shwetap3000 <shwetaprasad30.05@gmail.com>
2026-06-19 18:53:13 +0530

    implement role based access control

--- payrecon-backend/src/routes/authRoutes.js
+++ payrecon-backend/src/routes/authRoutes.js
@@ -1,12 +1,25 @@
 import express from "express";
-import { register, login, getMe, logout } from "../controllers/authController.js";
+import {
+  register,
+  login,
+  getMe,
+  logout,
+} from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import { authorizeRoles } from "../middleware/authorizeRole.js";
+
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
-router.post("/logout", logout)
+router.post("/logout", logout);
+router.get("/admin-test", protect, authorizeRoles("ADMIN"), (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome Admin",
+  });
+});
 
 export default router;
